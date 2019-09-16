a8ec966c6d549479d16ee9565617784ef9616e26
ballib <42639173+ballib@users.noreply.github.com>
2019-09-16 23:30:28 +0000

    .

--- project 4/move.py
+++ project 4/move.py
@@ -1,14 +1,31 @@
 pos = int((input("Input a position between 1 and 10: ")))
-string = ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x"]
+min = 1
+max = 10
+move1 = "r"
 
-string[pos-1] = "o"
+def position(i):
+    empty_string = ""
+    for x in range(min, max+1):
+        if x == i:
+            empty_string = empty_string + ("o")
+        else:
+            empty_string = empty_string + ("x")
+    print(empty_string)
 
+def move(y):
+    global pos
+    if y == ("r") and pos < max:
+        pos = pos + 1
 
-print(*string)
+    if y == ("l") and pos > min:
+        pos = pos - 1
 
-"""
+    position(pos)
+position(pos)
 print("l - for moving left")
 print("r - for moving right")
 print("Any other letter for quitting")
-move = input("Input your choice: ")
-"""
+
+while move1 == "r" or move1 == "l":
+    move1 = input("Input your choice: ")
+    move(move1)
