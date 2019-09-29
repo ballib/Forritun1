d90a05170118ca95a1b7bfba5fa53c41eef97966
ballib <42639173+ballib@users.noreply.github.com>
2019-09-29 23:53:57 +0000

    .

--- assignment 9/dæmi3.py
+++ assignment 9/dæmi3.py
@@ -1,14 +1,21 @@
-longest = ''
+def open_file(filename):
+    file_object = open(filename, "r")
+    return file_object
 
-with open("words.txt", "r", encoding = "utf-8") as text_file:
-    for word in text_file:
+def longest(file_object):
+    longest_word = ''
+    for word in file_object:
         strip = word.strip().replace(' ', '')
-        if len(strip) > len(longest):
-            longest = strip
+        if len(strip) > len(longest_word):
+            longest_word = strip
+    return longest_word
+
+def main():
+    filename = input("Enter filename: ")
+    file_object = open_file(filename)
+    longest_word = longest(file_object)
+    print("Longest word is",longest_word, "of length", len(longest_word))
 
 
-longest_length = len(longest)
 
-
-print(word)
-print("Longest word is", "'"+str(longest)+"'", "of length", str(longest_length))
+main()
