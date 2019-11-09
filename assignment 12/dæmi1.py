86ce06af9b73e1afec0229c644e2a0b4718c22aa
Baldur Bragi <baldurb2@gmail.com>
2019-11-09 17:21:11 +0000

    .

---
+++ assignment 12/dæmi1.py
@@ -0,0 +1,8 @@
+#definition for music_func goes here
+
+def main():
+    music, group, singer = input("Input music, group, singer: ").split(',')
+    music_func(music, group, singer)
+    music_func()
+
+main()
