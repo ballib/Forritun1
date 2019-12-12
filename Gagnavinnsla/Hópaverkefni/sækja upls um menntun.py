ab81ec705c93161de4596c5fe7f3d111c5a7432d
ballib <42639173+ballib@users.noreply.github.com>
2019-12-12 13:54:56 +0000

    .

--- Gagnavinnsla/Hópaverkefni/sækja upls um menntun.py
+++ Gagnavinnsla/Hópaverkefni/sækja upls um menntun.py
@@ -1,7 +1,7 @@
 import csv
 
 
-heroes = open("menntun.csv")
+heroes = open("menntun_nytt.csv")
 reader = csv.DictReader(heroes, delimiter=";")
 
 data = []
@@ -10,12 +10,12 @@ for d in reader:
     data.append(d)
 heroes.close()
 
-insert_menntun = "Insert into education (sex, age_group, education, year_2003, year_2004, year_2005, year_2006, year_2007, year_2008, year_2009, year_2010, year_2011) values ('{}', '{}', '{}', {}, {}, {}, {}, {}, {}, {}, {}, {}); \n"
+insert_menntun = "Insert into education (year, sex, age, total) values ({}, {}, {}, {}); \n"
 
 f = open('insertstatement_menntun.sql', 'w')
 
 
 for d in data:
-    f.write(insert_menntun.format(d['Kyn'], d['Aldursflokkur/búseta'], d['Menntun'], d['2003'], d['2004'], d['2005'], d['2006'], d['2007'], d['2008'], d['2009'], d['2010'], d['2011']))
+    f.write(insert_menntun.format(d['Ár'], d['Kyn'], d['Aldur'], d['AllirnemendurFjöldinemendaAlls']))
 
 f.close()
