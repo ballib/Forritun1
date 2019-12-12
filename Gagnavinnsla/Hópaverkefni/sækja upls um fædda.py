11ed3d8b566b7a432b9570452ec95dfec239ca54
ballib <42639173+ballib@users.noreply.github.com>
2019-12-12 20:19:43 +0000

    .

--- Gagnavinnsla/Hópaverkefni/sækja upls um fædda.py
+++ Gagnavinnsla/Hópaverkefni/sækja upls um fædda.py
@@ -1,7 +1,7 @@
 import csv
 
 
-born = open("fæddir_nytt.csv")
+born = open("FÆDDBÖRNAÐAL.csv")
 reader = csv.DictReader(born, delimiter=";")
 
 data = []
@@ -12,13 +12,13 @@ for d in reader:
 born.close()
 
 
-insert_birthrate = "Insert into birthrate (year, age15_44, age15_19, age20_24, age25_29, age30_34, age35_39, age40_44, age45_49) values ({}, {}, {}, {}, {}, {}, {}, {}, {}); \n"
+insert_birthrate = "Insert into birthrate (year, total, under20, age20_24, age25_29, age30_34, age35_39, age40_44, age45_49, over50) values ({}, {}, {}, {}, {}, {}, {}, {}, {}); \n"
 
 
-f = open('insertstatement_fæddir_nytt.sql', 'w')
+f = open('insertstatement_fæddir_aðal.sql', 'w')
 
 
 for d in data:
-    f.write(insert_birthrate.format(d['Ár'], d['15-44 ára'], d['15-19 ára'], d['20-24 ára'], d['25-29 ára'], d['30-34 ára'], d['35-39 ára'], d['40-44 ára'], d['45-49 ára']))
+    f.write(insert_birthrate.format(d['Ár'], d['Alls lifandi fÆdd bÖrn Alls'], d['Alls lifandi fÆdd bÖrn Innan 20 Ára'], d['Alls lifandi fÆdd bÖrn 20-24 Ára'], d['Alls lifandi FÆdd bÖrn 25-29 Ára'], d['Alls lifandi fædd börn 30-34 ára'], d['Alls lifandi fædd börn 35-39 ára'], d['Alls lifandi fædd börn 40-44 ára'], d['Alls lifandi fædd börn 45-49 ára'], d['Alls lifandi fÆdd bÖrn 50 Ára og eldri']))
 
 f.close()
