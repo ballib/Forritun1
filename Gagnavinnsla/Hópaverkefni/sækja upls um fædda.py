6f1a940c1ea415d7b812c37f3b301e80c14c01db
Baldur Bragi <baldurb2@gmail.com>
2019-12-12 16:38:42 +0000

    .

--- Gagnavinnsla/Hópaverkefni/sækja upls um fædda.py
+++ Gagnavinnsla/Hópaverkefni/sækja upls um fædda.py
@@ -2,7 +2,7 @@ import csv
 
 
 born = open("fæddir_nytt.csv")
-reader = csv.DictReader(born, delimiter=",")
+reader = csv.DictReader(born, delimiter=";")
 
 data = []
 
@@ -19,6 +19,6 @@ f = open('insertstatement_fæddir_nytt.sql', 'w')
 
 
 for d in data:
-    f.write(insert_birthrate.format(d['Ár'], d['"15-44 ára'], d['15-19 ára'], d['20-24 ára'], d['25-29 ára'], d['30-34 ára'], d['35-39 ára'], d['40-44 ára'], d['45-49 ára']))
+    f.write(insert_birthrate.format(d['Ár'], d['15-44 ára'], d['15-19 ára'], d['20-24 ára'], d['25-29 ára'], d['30-34 ára'], d['35-39 ára'], d['40-44 ára'], d['45-49 ára']))
 
 f.close()
